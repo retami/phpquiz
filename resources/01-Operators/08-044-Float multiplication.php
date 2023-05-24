4e94b5148e90a94849788fb9105b1c659c0bbf45
dev3139 <psatchy@gmail.com>
2023-05-24 10:44:49 +0200

    add whitespace

--- resources/01-Operators/08-044-Float multiplication.php
+++ resources/01-Operators/08-044-Float multiplication.php
@@ -5,13 +5,13 @@
 // What will be the output of the following PHP code?
 // php
 $n = "19.99";
-var_dump($n*100);
+var_dump($n * 100);
 
-var_dump((string) ($n*100));
-var_dump(strval($n*100));
+var_dump((string) ($n * 100));
+var_dump(strval($n * 100));
 
-var_dump((int) ($n*100));
-var_dump(intval($n*100));
+var_dump((int) ($n * 100));
+var_dump(intval($n * 100));
 // --
 // plain
 // float(1998.9999999999998)
