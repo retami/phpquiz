0bde3224357905a3d95ac97492e3210cd0744def
dev3139 <psatchy@gmail.com>
2023-05-24 19:44:11 +0200

    update layout

--- client/js/addBurgerToggle.js
+++ client/js/addBurgerToggle.js
@@ -3,7 +3,12 @@ import $ from 'jquery';
 export default function addBurgerToggle()
 {
     const hamburger = $("#burger");
+
     hamburger.click(function (e) {
+        if ($("#overlay").length == 0) {
+            $("body").prepend('<div id="overlay"></div>');
+        }
+        const overlay = $("#overlay");
         let open = e.target.dataset.open;
         if (open == null || open === '' || open === '0') {
             e.target.dataset.open = '1';
@@ -11,13 +16,15 @@ export default function addBurgerToggle()
             menu.attr('id', 'burger_menu');
             let content = $(".content_column");
             content.prepend(menu);
-            content.css('filter', 'brightness(50%)');
+            //content.css('filter', 'brightness(50%)');
+            overlay.addClass('visible');
         } else {
             this.dataset.open = '0';
             let menu = $("#burger_menu");
             menu.attr('id', 'menu_main');
             $(".menu_column").prepend(menu);
-            $(".content_column").css('filter', '');
+            //$(".content_column").css('filter', '');
+            overlay.removeClass('visible');
         }
     });
 }
